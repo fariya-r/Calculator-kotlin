a6db91be2249b1d5dc7276ca215226b007acf687
fariya-r <riyamuhammad01@gmail.com>
2024-07-04 13:17:48 +0500

    Add files via upload
    kotlin

---
+++ sum of squ of digits of a number.kt
@@ -0,0 +1,19 @@
+//45
+//Write a program that prompts the user to enter a positive integer
+// and then calculates and prints
+// the sum of the squares of each digit in that number using a do-while loop
+
+
+//    val obj = Scanner(System.`in`)
+//    var sum = 0
+//
+//    println("enter a positive integer to calculate its sum of square")
+//    var num = obj.nextInt()
+//
+//    do{
+//            var digit = num % 10
+//            sum+= digit*digit
+//            num /= 10
+//
+//    }while(num!=0)
+//    println("sum of squares is: $sum")
